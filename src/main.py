78ac31194cc9e73221691bf37476ddbc74fcaf6a
el0qu4nce <135351108+el0qu4nce@users.noreply.github.com>
2024-01-09 10:37:08 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from shop import Ui_Main_shop
 from checkout import Ui_Main_checkout
 from closing import Ui_Main_closing
 
+
 class Ya_App(QMainWindow):
     def __init__(self):
         super(Ya_App, self).__init__()
@@ -20,7 +21,6 @@ class Ya_App(QMainWindow):
         self.ui_closing = Ui_Main_closing()
 
     def open_shop(self):
-        # print('hello world')
         self.close()
         self.ui_shop.setupUi(self)
         self.show()
@@ -37,20 +37,9 @@ class Ya_App(QMainWindow):
         self.show()
         self.ui_checkout.pushButton.clicked.connect(lambda: self.open_closing())
 
-    def open_closing(self): #problem with design
+    def open_closing(self):
         self.close()
 
-    #     self.close()
-    #     self.ui_closing.setupUi(self)
-    #     self.show()
-    #     self.ui_shop
-    #
-    # def open_shop_1(self):
-    #     self.ui_shop_1 = Ui_Main_shop()
-    #     self.ui_shop_1.setupUi(self)
-    #     self.show()
-    #     print('task2')
-    #     # self.ui.pushButton_start.clicked.connect(lambda: self.open_shop())
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -58,42 +47,3 @@ if __name__ == "__main__":
     window.show()
 
     sys.exit(app.exec())
-
-
-# class Ya_shop(QMainWindow):
-#     def __init__(self):
-#         super(Ya_shop, self).__init__()
-#         self.ui = Ui_Main_opening()
-#         self.ui.setupUi(self)
-#         self.Ui_Main_shop = Ui_Main_shop
-#         self.Ui_Main_checkout = Ui_Main_checkout
-#         self.Ui_Main_closing = Ui_Main_closing
-#         # self.ui.pushButton.clicked.connect(self.open_shop())
-#         self.ui.pushButton.clicked.connect(self.open_shop())
-#         self.Ui_Main_shop = Ui_Main_shop
-#         self.ui.pushButton.clicked.connect(self.open_checkout())
-#         self.ui.pushButton.clicked.connect(self.open_closing())
-#
-#
-#     def open_shop(self): #функция вызова диалогового окна
-#         self.new_window = QtWidgets.QDialog()
-#         self.Ui_Main_shop.setupUi(self, self.new_window)
-#         self.new_window.show()
-#         # print('Yes')
-#
-#     def open_checkout(self):
-#         self.checkout_window = QtWidgets.QDialog()
-#         self.Ui_Main_checkout.setupUi(self, self.checkout_window)
-#         self.checkout_window.show()
-#
-#     def open_closing(self):
-#         self.close_window = QtWidgets.QDialog()
-#         self.Ui_Main_closing.setupUi(self, self.close_window)
-#         self.close_window.show()
-#
-# if __name__ == "__main__":
-#     app = QApplication(sys.argv)
-#     window = Ya_shop()
-#     window.show()
-#
-#     sys.exit(app.exec())
